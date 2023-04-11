837b957a15eb383f6f892c5adca829d4dc316496
JIYEONGSTAR <jiyeongstar@gmail.com>
2023-04-12 04:17:46 +0900

    [#22] feat: 게시글 id별로 불러오기

--- src/pages/[category]/[id].tsx
+++ src/pages/[category]/[id].tsx
@@ -1,9 +1,10 @@
-import { Category, IdProps } from "@/types";
+import { get } from "@/libs/api";
+import { Category, IdProps, MAIN } from "@/types";
 import { GetServerSideProps, GetServerSidePropsContext } from "next";
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import styled from "styled-components";
-
-const Detail = ({ category, id }: IdProps) => {
+import axios from "axios";
+const Detail = ({ data }: { data: MAIN }) => {
   const [showAlert, setShowAlert] = useState(false);
 
   const handleAButton = () => {
@@ -14,9 +15,31 @@ const Detail = ({ category, id }: IdProps) => {
     setShowAlert(false);
   };
 
+  useEffect(() => {
+    const getDate = async () => {
+      const data = await axios
+        .get(`/api/main/post?id=${"17"}`)
+        .then((res: any) => {
+          console.log(res);
+          return res.data;
+        });
+      console.log("Data", data);
+    };
+
+    getDate();
+  });
   return (
     <>
       <button onClick={handleAButton}>custom alert</button>
+      <div>{data.category}</div>
+      <div>{data.contact}</div>
+      <div>{data.context}</div>
+      <div>{data.createDate.toString()}</div>
+      <div>{data.endDate.toString()}</div>
+      <div>{data.images}</div>
+      <div>{data.nickname}</div>
+      <div>{data.title}</div>
+      <div>{data.view}</div>
       {showAlert && (
         <Modal>
           <p>글 작성 됐어요!</p>
@@ -24,20 +47,27 @@ const Detail = ({ category, id }: IdProps) => {
         </Modal>
       )}
     </>
-  );};
+  );
+};
 
 export default Detail;
-export const getServerSideProps: GetServerSideProps<IdProps> = async (
+export const getServerSideProps: GetServerSideProps<{ data: MAIN }> = async (
   context: GetServerSidePropsContext
 ) => {
   const { query } = context;
   const category = query.category as Category;
   const id = query.id as unknown as number;
 
+  const data = await get(
+    `http://api-skhumeet.duckdns.org/api/main/post?id=${id}`
+  ).then((res: any) => {
+    console.log(res);
+    return res.data;
+  });
+  console.log("data", data);
   return {
     props: {
-      category,
-      id,
+      data: data ?? null,
     },
   };
 };
@@ -54,8 +84,8 @@ const Modal = styled.div`
 
   background-color: white;
   border: 1px solid #999999;
-`
+`;
 
 const AlertBtn = styled.button`
   width: 50%;
-`
+`;
