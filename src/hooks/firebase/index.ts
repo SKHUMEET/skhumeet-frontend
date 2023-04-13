3f40ee9db1828b7e01c5dda2c3820a6372704b4a
JIYEONGSTAR <jiyeongstar@gmail.com>
2023-04-13 21:48:37 +0900

    refactor: firebase config 암호화

--- src/hooks/firebase/index.ts
+++ src/hooks/firebase/index.ts
@@ -2,8 +2,15 @@ import firebase from "firebase/compat/app";
 import "firebase/compat/auth";
 
 import { getStorage } from "firebase/storage";
-import "firebase/compat/auth";
-import firebaseconfig from "./firebaseconfig.json";
+
+const firebaseconfig = {
+  apiKey: process.env.NEXT_FIREBASE_APIKEY,
+  authDomain: process.env.NEXT_FIREBASE_AUTHDOMAIN,
+  projectId: process.env.NEXT_FIREBASE_PROJECTID,
+  storageBucket: process.env.NEXT_FIREBASE_STORAGEBUCKET,
+  messagingSenderId: process.env.NEXT_FIREBASE_MESSAGINGSENDERID,
+  appId: process.env.NEXT_FIREBASE_APPID,
+};
 
 const app = firebase.initializeApp(firebaseconfig);
 
