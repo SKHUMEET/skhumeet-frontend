df3b0696a3e59bef3460239812ef35cc24c449f6
JIYEONGSTAR <jiyeongstar@gmail.com>
2023-04-15 21:08:44 +0900

    feat: 새로 고침 시 창 사이드 바 스타일 수정

--- src/components/utils/ToggleUser.tsx
+++ src/components/utils/ToggleUser.tsx
@@ -9,7 +9,7 @@ import { User, storageConstants } from "@/types";
 const ToggleUser = () => {
   const router = useRouter();
 
-  const [isOpen, setIsOpen] = useState<boolean>(true);
+  const [isOpen, setIsOpen] = useState<boolean>(false);
 
   const [userImage, setUserImage] = useState<string>();
 
